5e8417912d28513ebd464cf1673b88c9d5e09858
carlos calderon <cdcalderon@gmail.com>
2015-10-20 02:46:20 -0500

    initial commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Carlos Calderon on 10/20/15.
+//  Copyright (c) 2015 Carlos Calderon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Carlos Calderon on 10/20/15.
+//  Copyright (c) 2015 Carlos Calderon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
